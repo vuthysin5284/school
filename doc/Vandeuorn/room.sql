2a4aee042166215d9e0a4528a4c977e466d6d1fb
vuthysin5284 <vuthy.sin5284@gmail.com>
2017-04-22 16:32:05 +0700

    vantheourn folder

---
+++ doc/Vandeuorn/room.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.9 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table aaiischo_ais1.room
+CREATE TABLE IF NOT EXISTS `room` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `floor_id` int(10) NOT NULL,
+  `room_number` varchar(30) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1',
+  `branch_id` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='this table store room information';
+
+-- Dumping data for table aaiischo_ais1.room: ~0 rows (approximately)
+DELETE FROM `room`;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
