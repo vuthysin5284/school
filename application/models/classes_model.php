ac3e3953f2ab18d087dd1d19de03356ead56839d
Horn BunEng <horn.buneng@gmail.com>
2017-05-14 14:27:50 +0700

    Classes insert updated deleted

---
+++ application/models/classes_model.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Classes_model extends CI_Model {
+		
+		function __construct()
+		{ 
+			parent::__construct();
+			$this->db = $this->load->database('default', TRUE); 
+			 
+		}  
+		/*create new classes*/
+		function new_classes($data){
+			$this->db->insert('classes',$data);
+			return $this->db->insert_id(); 		
+		}
+		/*edit classes*/
+		function edit_classes($data,$id){
+			$this->db->where('id',$id);
+			return $this->db->update('classes',$data);
+		}
+		/*delete classes*/
+		function delete_classes($obj){
+			$this->db->where('id',$obj->classes_id);
+			$this->db->set('is_delete',1);
+			$this->db->set('delete_by',$this->session->userdata("user_id"));
+			$this->db->set('delete_date',date('Y-m-d h:s:i'));
+			$this->db->update('classes');
+		}
+		/*udate status classes*/
+		function update_status_classes($obj){
+			$this->db->where('id',$obj->pricebook_id); 
+			$this->db->set('status',$obj->status);
+			$this->db->set('modified_by',$this->session->userdata("user_id"));
+			$this->db->set('modified_date',date('Y-m-d h:s:i'));
+			$this->db->update('classes');
+		}
+		
+		/* classes detail */
+		function get_classes_detail($obj){
+			$sql = " select 
+						*
+					from classes pb 
+					where id=?";
+			$data = $this->db->query($sql,array($obj->classes_id))->row();
+			return array( 
+                        "id"=> empty($data->id)?'':$data->id,
+                        "classes_number"=> empty($data->classes_number)?'':$data->classes_number,
+                        "classes_name"=> empty($data->classes_name)?'':$data->classes_name,
+                        "building"=> empty($data->building)?'':$data->building,
+                        "floor"=> empty($data->floor)?'':$data->floor,
+                        "description"=> empty($data->description)?'':$data->description,
+                        "status"=> empty($data->status)?'':$data->status,
+                        "created_date"=> empty($data->created_date)?'':$data->created_date,
+                        "modified_date"=> empty($data->modified_date)?'':$data->modified_date,
+                        "created_by"=> empty($data->created_by)?'':$data->created_by,
+                        "modified_by"=> empty($data->modified_by)?'':$data->modified_by,
+				);     
+		}
+		
+		/* lookup_classes */
+		function lookup_classes($obj){
+			$sql = "select * from classes where status = 1 and classes like ?";
+			return $this->db->query($sql,array($obj["keyword"].'%'))->result();
+		}
+		
+	}
+?>
--- application/views/classes/classes_list.php
+++ application/views/classes/classes_list.php
@@ -1,8 +1,6 @@
-<script src="<?php echo base_url();?>assets/js/classes/classes.js"></script>
-
 <div class="row">
     <div class="col-md-6 pull-right text-right">
-        <button class="btn btn-info" onclick="showAjaxModal('<?php echo base_url();?>classes/new_classes');">
+        <button class="btn btn-info" onclick="showAjaxModal('<?php echo base_url();?>classes/new_classes/0/new');">
             <i class="fa fa-user-plus"></i>
             <?php echo get_phrase('new_classes');?>
         </button>
@@ -14,12 +12,16 @@
 
 <table id="datable_classes" class="display" cellspacing="0" width="100%">
     <thead>
-    <tr>
-        <th>Classes Number</th>
-        <th>Classes Name</th>
-        <th>Description of Classes</th>
-        <th>STATUS</th>
-        <th></th>
-    </tr>
+        <tr>
+            <th>Classes Number</th>
+            <th>Classes Name</th>
+            <th>Building</th>
+            <th>Floor</th>
+            <th>Description</th>
+            <th>STATUS</th>
+            <th></th>
+        </tr>
     </thead>
 </table>
+
+<script src="<?php echo base_url();?>assets/js/classes/classes.js"></script>
