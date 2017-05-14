65717c339f2083256a6b4a1c3f938078a9f49cd4
vuthysin5284 <vuthy.sin5284@gmail.com>
2017-05-14 16:47:16 +0700

    data export

--- doc/Eng/buildingfloor/buildingfloor.sql
+++ doc/Eng/buildingfloor/buildingfloor.sql
@@ -21,18 +21,20 @@ CREATE TABLE IF NOT EXISTS `building` (
   `created_date` datetime DEFAULT NULL,
   `status` int(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Dumping data for table school.building: 0 rows
+-- Dumping data for table school.building: 1 rows
 DELETE FROM `building`;
 /*!40000 ALTER TABLE `building` DISABLE KEYS */;
+INSERT INTO `building` (`id`, `branch_id`, `building_name`, `description`, `created_by`, `created_date`, `status`) VALUES
+	(1, 1, 'Building A', 'Building A', 1, '2017-05-14 15:44:56', 1);
 /*!40000 ALTER TABLE `building` ENABLE KEYS */;
 
 -- Dumping structure for table school.floor
 CREATE TABLE IF NOT EXISTS `floor` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `branch_id` int(10) NOT NULL DEFAULT '0',
-  `floor` int(10) DEFAULT NULL,
+  `floor` varchar(50) DEFAULT NULL,
   `description` varchar(250) DEFAULT NULL,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT '0:inactive;1:active',
   PRIMARY KEY (`id`)
@@ -42,13 +44,13 @@ CREATE TABLE IF NOT EXISTS `floor` (
 DELETE FROM `floor`;
 /*!40000 ALTER TABLE `floor` DISABLE KEYS */;
 INSERT INTO `floor` (`id`, `branch_id`, `floor`, `description`, `status`) VALUES
-	(1, 0, 0, 'Ground Floor', 1),
-	(2, 0, 0, 'First Floor', 1),
-	(3, 0, 0, 'Second Floor', 1),
-	(4, 0, 0, 'Thirth Floor', 1),
-	(5, 0, 0, 'Four Floor', 1),
-	(6, 0, 0, 'Five Floor', 1),
-	(7, 0, 0, 'Six Floor', 1);
+	(1, 1, 'Ground Floor', 'Ground Floor', 1),
+	(2, 1, 'First Floor', 'First Floor', 1),
+	(3, 1, 'Second Floor', 'Second Floor', 1),
+	(4, 1, 'Thirth Floor', 'Thirth Floor', 1),
+	(5, 1, 'Four Floor', 'Four Floor', 1),
+	(6, 1, 'Five Floor', 'Five Floor', 1),
+	(7, 1, 'Six Floor', 'Six Floor', 1);
 /*!40000 ALTER TABLE `floor` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
