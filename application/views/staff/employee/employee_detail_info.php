a3fbad050a8d9d50836bba5e2f29d85a5ea94f09
Horn BunEng <horn.buneng@gmail.com>
2017-08-27 15:40:53 +0700

    Employee

--- application/views/staff/employee/employee_detail_info.php
+++ application/views/staff/employee/employee_detail_info.php
@@ -35,28 +35,74 @@
                            Contact
                            </a>
                 </li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Background</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Skills</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Language</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Experience</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Family</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">referrence</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Tracking</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Documents</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Internal Training</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Payslip</a></li>
-                <li role="presentation"><a href="<?php echo base_url();?>staff/employee_contact/1233"
-                                           data-url="<?php echo base_url();?>staff/employee_contact/1233" data-toggle="tab">Discipline</a></li>
+                <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_background/"
+                     data-url="<?php echo base_url();?>staff/employee_background/" data-toggle="tab" aria-expanded="false">
+       				 Background
+        			</a>
+       			 </li>
+                <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_skills/"
+                     data-url="<?php echo base_url();?>staff/employee_skills/" data-toggle="tab" aria-expanded="false">
+       				 Skills
+        			</a>
+        		</li>
+                <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_language/"
+                     data-url="<?php echo base_url();?>staff/employee_language/" data-toggle="tab" aria-expanded="false">
+       				 Language
+        			</a>
+        </li>
+         <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_experience/"
+                     data-url="<?php echo base_url();?>staff/employee_experience/" data-toggle="tab" aria-expanded="false">
+       				 Experience
+        			</a>
+        </li>
+                <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_family/"
+                     data-url="<?php echo base_url();?>staff/employee_family/" data-toggle="tab" aria-expanded="false">
+       				 Family
+        			</a>
+        </li>
+        <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_referrence/"
+                     data-url="<?php echo base_url();?>staff/employee_referrence/" data-toggle="tab" aria-expanded="false">
+       				 Referrence/
+        			</a>
+        </li>
+        <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_tracking/"
+                     data-url="<?php echo base_url();?>staff/employee_tracking/" data-toggle="tab" aria-expanded="false">
+       				 Tracking
+        			</a>
+        </li>
+        <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_document/"
+                     data-url="<?php echo base_url();?>staff/employee_document/" data-toggle="tab" aria-expanded="false">
+       				 Document
+        			</a>
+        </li>
+        <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_internal_training/"
+                     data-url="<?php echo base_url();?>staff/employee_internal_training/" data-toggle="tab" aria-expanded="false">
+       				 Internal Training
+        			</a>
+        </li>
+        
+       <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_payslip/"
+                     data-url="<?php echo base_url();?>staff/employee_payslip/" data-toggle="tab" aria-expanded="false">
+       				 Payslip
+        			</a>
+        </li>
+       
+        <li role="presentation">
+        		<a href="<?php echo base_url();?>staff/employee_discipline/"
+                     data-url="<?php echo base_url();?>staff/employee_discipline/" data-toggle="tab" aria-expanded="false">
+       				 Discipline
+        			</a>
+        </li>
             </ul>
             <!-- Tab panes -->
             <div class="col-md-12" style="padding-right:0px; padding-left: 0px;">
@@ -83,7 +129,7 @@
         <div class="form-actions pull-right" style="margin-right:20px;">
             <button type="button"  data-dismiss="modal" class="btn btn-info"><?php echo get_phrase('close');?></button>
             <button type="button" class="btn btn-info"><?php echo get_phrase('transfer');?></button>
-            <button type="button" class="btn btn-info"><?php echo get_phrase('transfer');?></button>
+            <button type="button" class="btn btn-info"><?php echo get_phrase('terminate');?></button>
             <button type="button" class="btn btn-info"><?php echo get_phrase('Review');?></button>
             <button type="button" id="btnSubmit" class="btn btn-info"><?php echo get_phrase('save');?></button>
         </div>
