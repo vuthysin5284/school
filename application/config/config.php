398d8846e24881b36241824fdd0dfe605d19ada1
moeunthul <bunthul12@gmail.com>
2019-09-30 16:46:16 +0700

    justify layout

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/eds/';
+$config['base_url'] = 'http://localhost:8080/eds/';
 // configure data
 $config['eds_sys'] = 'eds_sys';
 
